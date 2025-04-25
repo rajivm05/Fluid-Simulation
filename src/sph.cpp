7e370904bf7e42e1aa2fb6af3369dccfb56dd3b4
Rajiv Murali <rajivmurali2000@gmail.com>
2025-04-24 17:39:38 -0700

    basic2

--- src/sph.cpp
+++ src/sph.cpp
@@ -279,6 +279,16 @@ void SPH::create_cuboid(){
         glm::vec3(-lim_x,  lim_y, -lim_z),
         glm::vec3( lim_x,  lim_y,  lim_z),
         glm::vec3( lim_x,  lim_y, -lim_z),
+
+        // t11 - Front face
+        glm::vec3(-lim_x, -lim_y,  lim_z),
+        glm::vec3(-lim_x,  lim_y,  lim_z),
+        glm::vec3( lim_x,  lim_y,  lim_z),
+
+        // t12 - Front face
+        glm::vec3(-lim_x, -lim_y,  lim_z),
+        glm::vec3( lim_x,  lim_y,  lim_z),
+        glm::vec3( lim_x, -lim_y,  lim_z),
     };
     
 }
